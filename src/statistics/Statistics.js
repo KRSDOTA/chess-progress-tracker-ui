8b3ee8a4ecf0d40dd69340e91bd9f228b36e3fba
KRSProg <meepo0nly2@gmail.com>
2023-12-17 09:58:29 +0000

    added in a component for when no stats are present and added in some logic to conditionally show it

--- src/statistics/Statistics.js
+++ src/statistics/Statistics.js
@@ -4,6 +4,7 @@ import ChessBullet from "./chess_bullet/ChessBullet";
 import ChessRapid from "./chess_rapid/ChessRapid";
 import { useEffect, useState } from "react";
 import { getStatisticsForUsername } from "./StatisticsService";
+import NoStatsFound from "./NoStatsFound";
 
 export default function Statistics() {
     const [statsData, setStatsData] = useState(null);
@@ -25,9 +26,15 @@ export default function Statistics() {
 
     }
 
+    function noStatsPresent() {
+      return !statsData || (!statsData.chess_rapid && !statsData.chess_blitz && !statsData.chess_bullet);
+    }
+
     return (
       <div className="Statistics">
-        <SearchBar onSearchHandler={searchStatsApiHandler}/>
+        <h1>Displaying Game Stats for {searchQuery.toUpperCase()}</h1>
+        <SearchBar onSearchHandler={searchStatsApiHandler}/> 
+        {noStatsPresent() && <NoStatsFound />}
         { statsData && statsData.chess_rapid && <ChessRapid rapidData={statsData.chess_rapid} /> }
         { statsData && statsData.chess_blitz && <ChessBlitz blitzData={statsData.chess_blitz} /> }
         { statsData && statsData.chess_bullet && <ChessBullet bulletData={statsData.chess_bullet} /> }
