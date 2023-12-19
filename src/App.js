5424f2e54e1876eaf8661ec9698b7d2d245657a1
KRSProg <meepo0nly2@gmail.com>
2023-12-19 09:09:43 +0000

    Beginning to add an app bar at the top for site navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { Box } from '@mui/material';
 import './App.css';
 import Statistics from './statistics/Statistics';
+import SearchAppBar from './statistics/search/AppBar';
 
 function App() {
   return (
-    <>
+    <Box className="app-container">
+      <SearchAppBar />
       <Statistics />
-    </>
+    </Box>
   );
 }
 
---
+++ src/statistics/graph/ProgressChart.js
@@ -0,0 +1,8 @@
+/**
+ * Component for taking time period props and displaying 
+ * Graph data
+ */
+export default function ProgressChart() {
+    
+
+}
