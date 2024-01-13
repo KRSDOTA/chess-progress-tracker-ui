40a208f3aeeec0dfee5e52ee389621113f5c5fe1
KRSProg <meepo0nly2@gmail.com>
2024-01-13 06:58:34 +0000

    added in start and end date pickers which successfully call off to the API

--- src/statistics/Statistics.js
+++ src/statistics/Statistics.js
@@ -4,7 +4,7 @@ import ChessBlitz from "./chess_blitz/ChessBlitz";
 import ChessBullet from "./chess_bullet/ChessBullet";
 import ChessRapid from "./chess_rapid/ChessRapid";
 import { useState } from "react";
-import { getRatingDeltaAcrossTimePeriod, getRecentGamesForPlayer, getStatisticsForUsername } from "./StatisticsService";
+import { getRatingDeltaAcrossTimePeriod, getStatisticsForUsername } from "./StatisticsService";
 import NoStatsFound from "./NoStatsFound";
 import { Box } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
@@ -13,26 +13,43 @@ function StatisticsOverview() {
     const [statsData, setStatsData] = useState(null);
     const [searchQuery, setCurrentSearchQuery] = useState('');
     const [matchData, setCurrentMatchData] = useState([]);
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
 
-    function searchStatsApiHandler(searchQueryEvent) {
+    const searchStatsApiHandler = async (searchQueryEvent) => {
       const currentSearchQuery = searchQueryEvent.target.value;
       if(!currentSearchQuery){
         return;
       }
       
       setCurrentSearchQuery(currentSearchQuery);
-
-      getStatisticsForUsername(currentSearchQuery)
-      .then((response) => response.json())
-      .then(data => setStatsData(data));
-
-      getRatingDeltaAcrossTimePeriod(currentSearchQuery, "2023-01-01", "2023-12-31")
-      .then((response) => response.json())
-      .then(matchData => {setCurrentMatchData(matchData); console.log(matchData)});
-
+      
+      const response = await getStatisticsForUsername(currentSearchQuery);
+      const data = await response.json();
+      setStatsData(data);
     }
 
-    function noStatsPresent() {
+    const convertToUTCDate = (date) => new Date(date).toISOString().split('T')[0];
+
+    const getMatchData = async(date, id) => {
+      const utcDate = convertToUTCDate(date);
+      
+      if (id === "start") {
+        setStartDate(utcDate);
+      } else if (id === "end") {
+        setEndDate(utcDate);
+      }
+
+      if (startDate && endDate) {
+        const response = await getRatingDeltaAcrossTimePeriod(searchQuery, startDate, endDate);
+        const matchDataUpdate = await response.json();
+        console.log(matchDataUpdate);
+        setCurrentMatchData(matchDataUpdate);
+      }
+    }
+
+
+    const noStatsPresent = () => {
       return !statsData || (!statsData.chess_rapid && !statsData.chess_blitz && !statsData.chess_bullet);
     }
 
@@ -40,12 +57,8 @@ function StatisticsOverview() {
       <Box className="statistics-container">
         <h1>Displaying Game Stats for {searchQuery.toUpperCase()}</h1>
         <SearchBar onSearchHandler={searchStatsApiHandler}/>
-        <DatePicker />
-        <DatePicker />
-        {/* <Box className="date-picker-container">
-          <DatePickerStart />
-          <DatePickerEnd />
-        </Box>  */}
+        <DatePicker id="date-picker-start" onChange={(date) => getMatchData(date, "start")} />
+        <DatePicker id="date-picker-end" onChange={(date) => getMatchData(date, "end")} />
         <Box className='statistics-game-container'>
           {noStatsPresent() && <NoStatsFound />}
           { statsData && statsData.chess_rapid && <ChessRapid rapidData={statsData.chess_rapid} /> }
